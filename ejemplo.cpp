594017c441a20f4f77657f5e49ac5a4f2437afe1
ichoqueru <ichoquer@unjbg.edu.pe>
2025-06-18 23:30:41 -0500

    Actualizar ejemplo: agrega listar el número y nombre del empleado con mayor venta

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -9,25 +9,31 @@ struct EMP{
 
 int main(){
     EMP EMPLEADOS[100];
-    int n;
+    int n,mayor,mayorvent,sumavent;
     cout<<"Ingrese la cantidad de empleados: "; cin>>n;
     for(int i=0;i<n;i++){
-        cout<<"Numero del empleado: ";
-        cin>>EMPLEADOS[i].num;
+        cout<<"\nEmpleado #"<<i+1<<endl;
         cin.ignore();
         cout<<"Ingrese el nombre: ";
         getline(cin,EMPLEADOS[i].nom);
+
         for(int j=0; j<12;j++){
-            cout<<"Venta del mes: ";
-            cin>>EMPLEADOS[i].ven[j];            
+            cout<<"Venta del mes "<<j+1<<" : ";
+            cin>>EMPLEADOS[i].ven[j];  
+			sumavent=0;  
+			sumavent+=EMPLEADOS[i].ven[j];                       
         }
         cout<<"Ingrese el salario: ";
         cin>>EMPLEADOS[i].sal;
         cout<<endl;
+        if(sumavent>mayorvent || i==0){
+            	mayorvent=sumavent;
+            	mayor=i;
+        }
     }
+    cout<<"-Datos del empleado-\n";
       for(int i=0;i<n;i++){
-        cout<<"Cantidad de empleados: ";
-        cout<<EMPLEADOS[i].num<<endl;
+        cout<<"\nEmpleado #"<<i+1<<endl;
         cout<<"Nombre: ";
         cout<<EMPLEADOS[i].nom<<endl;
         cout<<"Ventas de cada mes: ";
@@ -37,5 +43,8 @@ int main(){
         cout<<"Salario: ";
         cout<<endl<<EMPLEADOS[i].sal<<endl<<endl;
     }
+    cout<<"\nEl empleado con mayores ventas del mes: \n";
+    cout<<"Nombre: "<<EMPLEADOS[mayor].nom<<endl;
+    cout<<"Ventas totales: "<<mayorvent<<endl;    
     return 0;
 }
