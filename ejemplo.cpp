4f53c4ba00ca9c44a923c202b807fcdd5e039b7d
ichoqueru <ichoquer@unjbg.edu.pe>
2025-06-18 22:41:20 -0500

    Actualizar ejemplo: Agrega mensajes para una mayor comprensión del código

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -10,22 +10,31 @@ struct EMP{
 int main(){
     EMP EMPLEADOS[100];
     int n;
-    cin>>n;
+    cout<<"Ingrese la cantidad de empleados: "; cin>>n;
     for(int i=0;i<n;i++){
+        cout<<"Numero del empleado: ";
         cin>>EMPLEADOS[i].num;
         cin.ignore();
+        cout<<"Ingrese el nombre: ";
         getline(cin,EMPLEADOS[i].nom);
         for(int j=0; j<12;j++){
+            cout<<"Venta del mes: ";
             cin>>EMPLEADOS[i].ven[j];            
         }
+        cout<<"Ingrese el salario: ";
         cin>>EMPLEADOS[i].sal;
+        cout<<endl;
     }
       for(int i=0;i<n;i++){
+        cout<<"Cantidad de empleados: ";
         cout<<EMPLEADOS[i].num<<endl;
+        cout<<"Nombre: ";
         cout<<EMPLEADOS[i].nom<<endl;
+        cout<<"Ventas de cada mes: ";
         for(int j=0; j<12;j++){
             cout<<EMPLEADOS[i].ven[j]<<" ";            
         }
+        cout<<"Salario: ";
         cout<<endl<<EMPLEADOS[i].sal<<endl<<endl;
     }
     return 0;
